5e9a4c750f10db39ef8c53f091227512ff052ea0
Aleks Mitrovic <mitrovic.aleksandar@outlook.com>
2021-01-06 16:05:15 -0500

    switched board to use numpy arrays to be more efficient

--- chess/engine.py
+++ chess/engine.py
@@ -1,16 +1,19 @@
 # We will store game information here and determine valid moves + keep a move log
+import numpy as np
+
+
 class State():
     def __init__(self):
         # can be more efficient if using numpy arrays for AI piece
-        self.board = [
-            ["bR", "bKn", "bB", "bQ", "bK", "bB", "bN", "bR"],
-            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
-            ["", "", "", "", "", "", "", "", ],
-            ["", "", "", "", "", "", "", "", ],
-            ["", "", "", "", "", "", "", "", ],
-            ["", "", "", "", "", "", "", "", ],
-            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"]
-            ["wR", "wKn", "wB", "wQ", "wK", "wB", "wN", "wR"],
-        ],
+        self.board = np.array([
+            np.array(["bR", "bKn", "bB", "bQ", "bK", "bB", "bN", "bR"]),
+            np.array(["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"]),
+            np.array(["", "", "", "", "", "", "", "", ]),
+            np.array(["", "", "", "", "", "", "", "", ]),
+            np.array(["", "", "", "", "", "", "", "", ]),
+            np.array(["", "", "", "", "", "", "", "", ]),
+            np.array(["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"]),
+            np.array(["wR", "wKn", "wB", "wQ", "wK", "wB", "wN", "wR"]),
+        ])
         self.whiteMove = True
         self.log = []
--- chess/main.py
+++ chess/main.py
@@ -1,1 +1,13 @@
-#Handling user input and displaying the board (state of the game, i.e. State class)
+# Handling user input and displaying the board (state of the game, i.e. State class)
+import pygame as p
+from chess import engine
+
+WIDTH = HEIGHT = 400
+DIMENSION = 8
+SQUARE_SIZE = WIDTH // DIMENSION
+MAX_FPS = 15  # for animations only
+IMAGES = {}
+
+'''
+
+'''
