38ec9c4f223be2cd1f3201003edcd1df6afda85b
Aleks Mitrovic <mitrovic.aleksandar@outlook.com>
2021-01-06 15:50:11 -0500

    created State class with constructor, board, white move, and log

--- chess/engine.py
+++ chess/engine.py
@@ -1,1 +1,16 @@
-#We will store game information here and determine valid moves + keep a move log
+# We will store game information here and determine valid moves + keep a move log
+class State():
+    def __init__(self):
+        # can be more efficient if using numpy arrays for AI piece
+        self.board = [
+            ["bR", "bKn", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["", "", "", "", "", "", "", "", ],
+            ["", "", "", "", "", "", "", "", ],
+            ["", "", "", "", "", "", "", "", ],
+            ["", "", "", "", "", "", "", "", ],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"]
+            ["wR", "wKn", "wB", "wQ", "wK", "wB", "wN", "wR"],
+        ],
+        self.whiteMove = True
+        self.log = []
