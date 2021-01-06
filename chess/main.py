8d6a9bec55d5c5dbb11b25ddf9a43a7fbb6515f2
Aleks Mitrovic <mitrovic.aleksandar@outlook.com>
2021-01-06 16:51:02 -0500

    opens separate window for board

--- chess/main.py
+++ chess/main.py
@@ -2,16 +2,40 @@
 import pygame as pg
 import engine
 
+pg.init()  # initializing pygame
 WIDTH = HEIGHT = 400
-DIMENSION = 8
-SQUARE_SIZE = WIDTH // DIMENSION
+SQUARE_SIZE = WIDTH // 8
 MAX_FPS = 15  # for animations only
 IMAGES = {}
 
 
 def import_images():
+    """Only used once to import piece images at the start of the game"""
     pieces = ["wR", "wKn", "wB", "wQ", "wK",
               "wP", "bR", "bKn", "bB", "bQ", "bK", "bP"]
     for piece in pieces:
         IMAGES[piece] = pg.transform.scale(pg.image.load(
             "images/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
+
+
+def main():
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    screen.fill(pg.Color("White"))
+
+    clock = pg.time.Clock()
+    state = engine.State()
+
+    import_images()
+
+    playing = True
+    while playing:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                playing = False
+        clock.tick(MAX_FPS)
+        pg.display.flip()
+
+
+# this will allow main.py to run only when we're running this module, not when it's imported from another one
+if __name__ == "__main__":
+    main()
