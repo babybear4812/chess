139872ef4a82efa3a8964d1dd117cd3c235dee83
Aleks Mitrovic <mitrovic.aleksandar@outlook.com>
2021-01-06 17:19:20 -0500

    refactored logic in drawing board

--- chess/main.py
+++ chess/main.py
@@ -4,7 +4,7 @@ import engine
 
 pg.init()  # initializing pygame
 WIDTH = HEIGHT = 400
-SQUARE_SIZE = WIDTH // 8
+SQ_SIZE = WIDTH // 8
 MAX_FPS = 15  # for animations only
 IMAGES = {}
 
@@ -15,7 +15,7 @@ def import_images():
               "wP", "bR", "bKn", "bB", "bQ", "bK", "bP"]
     for piece in pieces:
         IMAGES[piece] = pg.transform.scale(pg.image.load(
-            "images/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
+            "images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
 
 
 def main():
@@ -32,8 +32,27 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 playing = False
+        drawState(screen, state)
         clock.tick(MAX_FPS)
         pg.display.flip()
+
+
+def drawState(screen, state):
+    board = state.board
+
+    def drawBoard():
+        for i in range(8):
+            for j in range(8):
+                color = pg.Color(235, 235, 208) if (
+                    i + j) % 2 == 0 else pg.Color(119, 148, 85)
+                pg.draw.rect(screen, color, pg.Rect(
+                    j*SQ_SIZE, i*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+    def drawPieces():
+        pass
+
+    drawBoard()
+    drawPieces()
 
 
 # this will allow main.py to run only when we're running this module, not when it's imported from another one
