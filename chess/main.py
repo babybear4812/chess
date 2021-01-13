b7e32cb8a22fc3a9eaa3fb40065c70fba4873290
Aleks Mitrovic <mitrovic.aleksandar@outlook.com>
2021-01-12 19:45:17 -0500

    added reset function by pressing the r key

--- chess/main.py
+++ chess/main.py
@@ -124,10 +124,18 @@ def main():
                         prevClicks = [sqClicked]
 
             elif event.type == pg.KEYDOWN:
+                # key listener for undo move
                 if event.key == pg.K_z:
                     state.undo_move()
                     # we will consider this a move made so that it will trigger validMove recalculation
                     moveMade = True
+                # key listener for restart game
+                elif event.key == pg.K_r:
+                    state = engine.State()
+                    validMoves = state.get_valid_moves
+                    sqClicked = ()
+                    prevClicks = []
+                    moveMade = False
 
         # if a move was made, generate new set of valid moves and reset flag
         if moveMade:
